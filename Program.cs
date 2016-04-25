d92a13c25f59a34fd03b03183518973e06a7b5e7
heununes <heu.nunes@gmail.com>
2016-04-25 11:54:44 +0100

    project start - rssFeed
    added rssFeed engine
    added database project

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace poiEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String url = "http://www.formacaosaude.com/feed/";
+            String[,] rssData = getRssFeeds(url);
+
+            for (int i=0; i < rssData.GetLength(0); i++)
+            {
+                for(int j=0; j < 2; j++)
+                {
+                    Console.WriteLine(rssData[i, j]);
+                    Console.WriteLine("\n");
+                }
+                
+            }
+        }
+
+        private static String[,] getRssFeeds(String Channel)
+        {
+            String[,] tempRssData = new String[100, 3];
+            XmlReader reader = XmlReader.Create(Channel);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                String subject = item.Title.Text;
+                String summary = item.Summary.Text;
+            }
+
+            return tempRssData;
+        }
+    }
+}
